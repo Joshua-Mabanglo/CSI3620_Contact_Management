8a0c161e0989fb4f036d0ce6378213e43b1f31cb
Anthony Rurka <113866688+AnthonyRurka@users.noreply.github.com>
2026-04-12 19:27:44 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,11 +2,13 @@ import Contact
 from ContactBST import ContactBST
 
 bst = ContactBST()
+hash_table = ContactHashTable()
 
 while True:
     print("\n1: Add contact")
-    print("2: Delete contact")
-    print("3: Exit")
+    print("2: Search by phone")
+    print("3: Search by email")
+    print("4: Exit")
     print("-" * 20)
 
     decision = int(input("Type 1 to add, 2 to delete, 3 to exit: "))
@@ -22,17 +24,33 @@ while True:
         print("Contact " + contact.name + " made.")
         
         bst.insert(contact)
+        hash_table.insert(contact)
             
         print("\nSorted contacts (BST):")
         bst.display_sorted()
         
-    elif(decision == 2):
-        print("Delete not implemented yet.")
-        
-    elif decision == 3:
-        print("Exiting program...")
-        break 
-        
+    
+elif decision == 2:
+    phone = input("Enter phone: ")
+    result = hash_table.search_by_phone(phone)
+
+    if result:
+        print("Found:", result.name, result.phone, result.email)
+    else:
+        print("Contact not found.")
+
+elif decision == 3:
+    email = input("Enter email: ")
+    result = hash_table.search_by_email(email)
+
+    if result:
+        print("Found:", result.name, result.phone, result.email)
+    else:
+        print("Contact not found.")
+
+elif decision == 4:
+    print("Exiting program...")
+    break
 
 
 
